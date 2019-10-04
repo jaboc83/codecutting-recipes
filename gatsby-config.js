23d059dc4264dcc76c254e99907c8edaf2d05ec2
Jake Moening <jmoening@unsignedbytes.com>
2019-10-03 21:02:57 -0500

    Add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter (Tailwind)`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,5 +32,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  //pathPrefix: "/codecutting-recipes",
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header className="bg-green-600 mb-1">
+    <div className="m-auto max-w-4xl py-4">
+      <h1 className="m-0 text-2xl">
+        <Link to="/" className="text-white no-underline">
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
